ac3b79dd27c54a734eb966bc3039da84ac0b3cb0
R3tr0-99 <alessiorago99@github.com>
2024-06-18 18:07:55 +0200

    Added phyisics to the ground

--- src/Ground.jsx
+++ src/Ground.jsx
@@ -1,15 +1,19 @@
 import * as THREE from "three";
 import { useTexture } from "@react-three/drei";
 import floorTexture from "./assets/floor3.png";
+import { RigidBody } from "@react-three/rapier";
 
 export const Ground = () => {
     const texture= useTexture(floorTexture);
     texture.wrapS= texture.WrapT= THREE.RepeatWrapping;
     
     return (
-        <mesh position={[0, -5, 0]} rotation-x={-Math.PI / 2}>
-            <planeGeometry args={[500, 500]} />
-            <meshStandardMaterial color="grey" map={texture} map-repeat={[100, 100]} />
-        </mesh>
+        /*Physics for the ground*/
+        <RigidBody>
+            <mesh position={[0, -5, 0]} rotation-x={-Math.PI / 2}>
+              <planeGeometry args={[500, 500]} />
+              <meshStandardMaterial color="grey" map={texture} map-repeat={[100, 100]} />
+            </mesh>
+        </RigidBody>
     );
 }
