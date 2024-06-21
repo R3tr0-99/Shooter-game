965fd775014d222c7aed2f0107c894a3c06c7d15
R3tr0-99 <alessiorago99@gmail.com>
2024-06-21 12:35:37 +0200

    Added first weapon and shadows

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { Ground } from "./Ground.jsx"
 import { Physics } from "@react-three/rapier";
 import { Player } from "./Player.jsx";
 import { Cubes } from "./Cube.jsx";
+import { WeaponModel } from "./WeaponModel.jsx";
 
 export const App = () => {
   return (
@@ -11,13 +12,19 @@ export const App = () => {
      <PointerLockControls />
      <Sky sunPosition={[100, 20, 100]} />
      <ambientLight intensity={1.5} />
+     <directionalLight castShadow intensity={.8} position={[50, 50, 0]} />
 
      {/* gravity effects and cube */}
      <Physics gravity={[0, -20, 0]}>
-      <Ground />
-      <Player />
-      <Cubes />
+        <Ground />
+        <Player />
+        <Cubes />
      </Physics>
+
+     {/* assault rifle */}
+     <group position={[3, 1, -2]}>
+        <WeaponModel />
+     </group>
     </>
   )
 }
